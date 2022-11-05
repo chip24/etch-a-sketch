b9195feacec5f54f7c84cee2a51d3de9d3486340
Chip Morgan <python.chipmorgan@gmail.com>
2022-11-05 09:05:02 -0500

    add button and prompt user for number

--- easscript.js
+++ easscript.js
@@ -21,9 +21,19 @@
 
 grid(document.body);*/
 
+async function promptNumber(){
+  var boxString = prompt("Enter a number of between 2 and 50");
+  console.log(boxString);
+  console.log(typeof boxString);
+  var boxNumber = (+boxString);
+  console.log(typeof boxNumber);
+  return(boxNumber);
+}
+
 const container = document.querySelector('#container');
 
-function createBoxes(numBox) {
+async function createBoxes(numBox) {
+  await promptNumber();
   for (let i = 0; i < numBox; i++) {
     const row = container.appendChild(document.createElement('div'));
     for (let j = 0; j < numBox; j++) {
@@ -35,4 +45,21 @@ function createBoxes(numBox) {
 }
 
 
-createBoxes(25);
+//async function createBoxes(boxNumber) {
+//  console.log(boxNumber);
+ // await promptNumber();
+//};
+
+createBoxes(boxNumber);
+
+//const cont = document.getElements("container");
+const box = document.getElementsByClassName("box");
+
+for (let i = 0; i < box.length; i++){
+  box[i].onmouseover = function(e){
+    box[i].style.backgroundColor = "blue";
+  }
+  
+}
+
+
